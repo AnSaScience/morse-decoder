69c51fdcecd8f4f496e3bf4dcb875d7800cdaa87
AnSaScience <ansascience@gmail.com>
2022-02-18 01:07:58 +0200

    init: start task morze decoder; feat: realese task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morzeCode= expr.match(/.{1,10}/g).map((x,i)=> { return x.match(/.{1,2}/g).filter(y=> {
+        if(y==="11" || y==="**" || y==="10")
+        {return true}
+       
+        else{
+          return false
+        }
+        
+      }).map(x=>{
+        if(x==="10"){
+          return "."
+        }
+        else if(x==="11"){
+          return "-"
+        }
+        else if(x==="**"){
+          return " "
+        }
+      })})
+      let morzeLeters=morzeCode.map(x=> {return x.join("")})
+      let answer=morzeLeters.map(x=>{
+         if(MORSE_TABLE[x]===undefined){
+          return " ";
+        }
+        else{
+          return MORSE_TABLE[x]
+        }
+      })
+      return answer.join("")
 }
 
 module.exports = {
